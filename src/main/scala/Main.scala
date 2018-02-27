5c6c3ebbaca5beea988502f226cb9216a707c415
Thomas Schwabauer <thomas.schwabauer@mni.thm.de>
2018-02-27 20:59:23 +0100

    paar tests mit puck

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,7 +6,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     var pfad = "PuckTest//"
-    var puckFile = scala.io.Source.fromFile(pfad + "test.puck").mkString
+    var puckFile = scala.io.Source.fromFile(pfad + "proc.puck").mkString
    var parsed = ProgParsers.parse(puckFile)
     println(parsed)
   }
