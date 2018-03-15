8483455c35f655d2dcc2f4eece0e74f5c38d999a
Thomas Schwabauer <thomas.schwabauer@mni.thm.de>
2018-03-16 00:36:14 +0100

    final

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -12,21 +12,21 @@ import scala.util.Try
 object Main {
 
   def main(args: Array[String]): Unit = {
-    /*if (args.length != 1) {
+    if (args.length != 1) {
       println("Command line arguments invalid")
       return
-    }*/
-    var pfad = "PuckTest//puck//"
-    var puckFile = scala.io.Source.fromFile(pfad + "test.puck").mkString
-    //var puckFile = scala.io.Source.fromFile(args(0)).mkString
+    }
+    //var pfad = "PuckTest//puck//"
+    //var puckFile = scala.io.Source.fromFile(pfad + "test.puck").mkString
+    var puckFile = scala.io.Source.fromFile(args(0)).mkString
     var parsed = Try(ProgParsers.parse(puckFile))
     if(parsed.isSuccess) {
       var analysed = Try(ContextAnalysis.checkContext(parsed.get))
       if(analysed.isSuccess){
         var zwischenCode = ZwischenCode.ZwischenCodeGenerator.translate(analysed.get)
         var assembler = CodeGenerator.GenAssemblerLines.gen(zwischenCode)
-        new PrintWriter("PuckTest//TestDateien//Test.a") {
-          //args(0).replaceFirst("[.][^.]+$", ".o")
+        new PrintWriter(args(0).replaceFirst("[.][^.]+$", ".o")) {
+          //"PuckTest//TestDateien//Test.a"
           write(AbstractSyntaxPrinter.apply(assembler))
           close()
         }
